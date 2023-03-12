d119f0a100bbc9d38aca08dac8b6fdccf097ebe1
Katriel Tsepelevish <katriel.tsepelevish@gmail.com>
2023-03-12 16:56:18 +0200

    fix: Button unnecessary outline

--- packages/client/src/modules/dashboard/components/Monitors/AddMonitor/AddMonitorButton.tsx
+++ packages/client/src/modules/dashboard/components/Monitors/AddMonitor/AddMonitorButton.tsx
@@ -8,7 +8,7 @@ interface AddMonitorButtonProps
 const AddMonitorButton: React.FC<AddMonitorButtonProps> = ({ ...props }) => (
   <div>
     <button
-      className="fixed bottom-0 right-0 m-4 p-2 bg-[#dedede] hover:bg-[#eeeeee] rounded-full z-10 shadow-sm transition-all duration-500"
+      className="fixed bottom-0 right-0 m-4 p-2 bg-[#dedede] hover:bg-[#eeeeee] rounded-full z-10 shadow-sm transition-all duration-500 outline-none"
       {...props}
     >
       <RiAddLine className="text-[26px]" />
